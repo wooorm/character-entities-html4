65386beca2b02568a8564a3c61e348f5da8a6ade
Titus Wormer <tituswormer@gmail.com>
2022-11-16 11:11:48 +0100

    Refactor build script

--- build.js
+++ build.js
@@ -1,46 +1,36 @@
-import fs from 'node:fs'
-import https from 'node:https'
-import {bail} from 'bail'
-import concatStream from 'concat-stream'
+import fs from 'node:fs/promises'
+import fetch from 'node-fetch'
 
-https.get('https://www.w3.org/TR/html4/sgml/entities.html', (response) => {
-  response.pipe(concatStream(onconcat)).on('error', bail)
-})
+const response = await fetch('https://www.w3.org/TR/html4/sgml/entities.html')
+const text = await response.text()
 
-/**
- * @param {Buffer} data
- */
-function onconcat(data) {
-  const value = String(data)
-  /** @type {Record.<string, string>} */
-  const entities = {}
-  const re = /&lt;!ENTITY([\s\S]+?)--&gt;/g
-  let match = re.exec(value)
+/** @type {Record<string, string>} */
+const entities = {}
+const re = /&lt;!ENTITY([\s\S]+?)--&gt;/g
+let match = re.exec(text)
 
-  while (match) {
-    const list = match[1].split('--', 1)[0].split(/\s+/).filter(Boolean)
+while (match) {
+  const list = match[1].split('--', 1)[0].split(/\s+/).filter(Boolean)
 
-    if (list[1] === 'CDATA') {
-      entities[list[0]] = String.fromCodePoint(
-        Number(list[2].split('#')[1].split(';')[0])
-      )
-    }
-
-    match = re.exec(value)
+  if (list[1] === 'CDATA') {
+    entities[list[0]] = String.fromCodePoint(
+      Number(list[2].split('#')[1].split(';')[0])
+    )
   }
 
-  fs.writeFile(
-    'index.js',
-    [
-      '/**',
-      ' * Map of named character references from HTML 4.',
-      ' *',
-      ' * @type {Record<string, string>}',
-      ' */',
-      'export const characterEntitiesHtml4 = ' +
-        JSON.stringify(entities, null, 2),
-      ''
-    ].join('\n'),
-    bail
-  )
+  match = re.exec(text)
 }
+
+await fs.writeFile(
+  'index.js',
+  [
+    '/**',
+    ' * Map of named character references from HTML 4.',
+    ' *',
+    ' * @type {Record<string, string>}',
+    ' */',
+    'export const characterEntitiesHtml4 = ' +
+      JSON.stringify(entities, null, 2),
+    ''
+  ].join('\n')
+)
