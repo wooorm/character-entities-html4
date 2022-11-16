da194006c437242e4fbebd0af78b1b673158fafd
Titus Wormer <tituswormer@gmail.com>
2022-11-16 11:10:33 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {characterEntitiesHtml4} from './index.js'
 
-test('characterEntitiesHtml4', function (t) {
-  t.equal(characterEntitiesHtml4.AElig, 'Æ')
-  t.equal(characterEntitiesHtml4.aelig, 'æ')
-  t.equal(characterEntitiesHtml4.amp, '&')
-  t.equal(characterEntitiesHtml4.apos, undefined)
-
-  t.end()
+test('characterEntitiesHtml4', function () {
+  assert.equal(characterEntitiesHtml4.AElig, 'Æ')
+  assert.equal(characterEntitiesHtml4.aelig, 'æ')
+  assert.equal(characterEntitiesHtml4.amp, '&')
+  assert.equal(characterEntitiesHtml4.apos, undefined)
 })
